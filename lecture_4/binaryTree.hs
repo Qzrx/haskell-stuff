29a35e021c69b0c665eb9788048a6dd8b82d2c90
Robert Beebe <rbeebe@lm-sfo-00870996.corp.ebay.com>
2013-07-15 15:14:03 -0700

    it works!

--- lecture_4/binaryTree.hs
+++ lecture_4/binaryTree.hs
@@ -2,6 +2,6 @@ data Tree a = EmptyTree
             | NodeValue a (Tree a) (Tree a) 
             deriving (Show, Eq)
  
---add :: Tree -> Int
+add :: (Num a) => Tree a -> a
 add (EmptyTree)       = 0
 add (NodeValue a l r) =  a + add (l) + add (r)
